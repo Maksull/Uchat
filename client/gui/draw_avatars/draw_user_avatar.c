a14312634bc809dde000be60f96f9820a0401bba
alex.boltenkov.yostix <alexandr.boltenkov@yostix.com>
2024-03-28 17:16:56 +0200

    gui structure update

--- client/gui/draw_avatars/draw_user_avatar.c
+++ client/gui/draw_avatars/draw_user_avatar.c
@@ -1,19 +1,19 @@
-#include "../inc/client.h"
+#include "../../inc/client.h"
 
-// Function to draw chat avatar
-gboolean draw_chat_avatar(GtkWidget *widget, cairo_t *cr, gpointer data)
+// Function to draw user avatar
+gboolean draw_user_avatar(GtkWidget *widget, cairo_t *cr, gpointer data)
 {
     if (widget) { }
     int avatar_color = GPOINTER_TO_INT(data); // Extract the avatar color from the data
 
-    // Construct the path to the chat avatar image file based on the color
-    char *path = "client/data/img/chat_avatars/avatar";
+    // Construct the path to the avatar image file based on the color
+    char *path = "client/data/img/user_avatars/avatar";
     char *tmp = mx_strjoin(path, mx_itoa(avatar_color + 1));
     path = mx_strjoin(tmp, ".png");
     mx_strdel(&tmp);
 
-    // Load the chat avatar image and set it as the source for Cairo
-    GdkPixbuf *pixbuf = gdk_pixbuf_new_from_file_at_scale(path, 42, 42, FALSE, NULL);
+    // Load the avatar image and set it as the source for Cairo
+    GdkPixbuf *pixbuf = gdk_pixbuf_new_from_file_at_scale(path, 27, 27, FALSE, NULL);
     mx_strdel(&path);
     gdk_cairo_set_source_pixbuf(cr, pixbuf, 0, 0);
     g_object_unref(G_OBJECT(pixbuf));
@@ -21,8 +21,8 @@ gboolean draw_chat_avatar(GtkWidget *widget, cairo_t *cr, gpointer data)
     // Draw a rounded rectangle to clip the avatar
     double x = 0;
     double y = 0;
-    double width = 42;
-    double height = 42;
+    double width = 27;
+    double height = 27;
     double aspect = 1.0;
     double corner_radius = height / 2.0;
     double radius = corner_radius / aspect;
@@ -35,6 +35,7 @@ gboolean draw_chat_avatar(GtkWidget *widget, cairo_t *cr, gpointer data)
     cairo_arc(cr, x + radius, y + radius, radius, 180 * degrees, 270 * degrees);
     cairo_close_path(cr);
 
-    cairo_fill(cr); // Fill the clipped area with the chat avatar image
+    // Fill the clipped area with the avatar image
+    cairo_fill(cr);
     return FALSE;
 }
