1151456db4da05706c937cf955dfbe78990fed5d
Maksull <kyrkyrkyr99@gmail.com>
2024-04-12 14:48:10 +0300

    Refactor message handlers

--- server/handlers/handlers_message/handle_delete_message.c
+++ server/handlers/handlers_message/handle_delete_message.c
@@ -1,21 +1,42 @@
 #include "../../inc/server.h"
 
+// Function to initialize the database and handle initialization errors
+static int initialize_database(t_server_utils *utils)
+{
+    if (init_db() != 0)
+    {
+        send_server_response(utils->ssl, R_DB_FAILURE, REQ_DELETE_MESSAGE);
+        return 1; // Error
+    }
+
+    return 0; // Success
+}
+
+// Function to delete the message from the database and handle errors
+static int delete_message_from_database(const cJSON *message_info, t_server_utils *utils)
+{
+    t_response_code resp_code = 0;
+    if ((resp_code = db_delete_message(message_info, utils)) != R_SUCCESS)
+    {
+        send_server_response(utils->ssl, resp_code, REQ_DELETE_MESSAGE);
+        return 1; // Error
+    }
+
+    return 0; // Success
+}
+
 // Function to handle the deletion of a message
 void handle_delete_message(const cJSON *message_info, t_server_utils *utils)
 {
     // Initialize the database and check for initialization errors
-    if (init_db() != 0)
+    if (initialize_database(utils) != 0)
     {
-        send_server_response(utils->ssl, R_DB_FAILURE, REQ_DELETE_MESSAGE);
         return;
     }
 
-    t_response_code resp_code = 0;
-
     // Delete the message from the database and check for errors
-    if ((resp_code = db_delete_message(message_info, utils)) != R_SUCCESS)
+    if (delete_message_from_database(message_info, utils) != 0)
     {
-        send_server_response(utils->ssl, resp_code, REQ_DELETE_MESSAGE);
         return;
     }
 
--- server/handlers/handlers_message/handle_last_message_id.c
+++ server/handlers/handlers_message/handle_last_message_id.c
@@ -1,27 +1,36 @@
 #include "../../inc/server.h"
 
-// Function to handle the retrieval of the ID of the last message in a chat
-void handle_last_msg_id(const cJSON *chat_info, t_server_utils *utils)
+// Function to initialize the database and handle initialization errors
+static int initialize_database(t_server_utils *utils, t_request_type request_type)
 {
-    // Initialize the database and check for initialization errors
     if (init_db() != 0)
     {
-        send_server_response(utils->ssl, R_DB_FAILURE, REQ_LAST_MSG_ID);
-        return;
+        send_server_response(utils->ssl, R_DB_FAILURE, request_type);
+
+        return 1; // Error
     }
 
-    int last_msg_id = -1;
-    int error_code = 0;
+    return 0; // Success
+}
 
-    // Retrieve the last message ID and check for errors
-    if ((error_code = db_get_last_message_id(chat_info, &last_msg_id)) != R_SUCCESS)
+// Function to retrieve the ID of the last message in a chat and handle errors
+static int retrieve_last_message_id(const cJSON *chat_info, int *last_msg_id, t_server_utils *utils)
+{
+    int error_code = db_get_last_message_id(chat_info, last_msg_id);
+    if (error_code != R_SUCCESS)
     {
         send_server_response(utils->ssl, error_code, REQ_LAST_MSG_ID);
-        return;
-    }
 
+        return 1; // Error
+    }
+    
+    return 0; // Success
+}
+
+// Function to create and send the response JSON object
+static void send_response_json(int last_msg_id, t_server_utils *utils)
+{
     cJSON *json = cJSON_CreateObject();
-    // Add the last message ID, type and error_code fields to the response JSON object
     cJSON_AddNumberToObject(json, "last_msg_id", last_msg_id);
     cJSON_AddNumberToObject(json, "type", REQ_LAST_MSG_ID);
     cJSON_AddNumberToObject(json, "error_code", R_SUCCESS);
@@ -30,4 +39,25 @@ void handle_last_msg_id(const cJSON *chat_info, t_server_utils *utils)
     send_response_to(utils->ssl, json_str);
     cJSON_Delete(json);
     free(json_str);
+}
+
+// Function to handle the retrieval of the ID of the last message in a chat
+void handle_last_msg_id(const cJSON *chat_info, t_server_utils *utils)
+{
+    // Initialize the database and check for initialization errors
+    if (initialize_database(utils, REQ_LAST_MSG_ID) != 0)
+    {
+        return;
+    }
+
+    int last_msg_id = -1;
+
+    // Retrieve the last message ID and check for errors
+    if (retrieve_last_message_id(chat_info, &last_msg_id, utils) != 0)
+    {
+        return;
+    }
+
+    // Send the response JSON object
+    send_response_json(last_msg_id, utils);
 }
