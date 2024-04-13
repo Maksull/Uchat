e4911fde723c21cc5c00b5dbd9062d098b0bf492
Maksull <kyrkyrkyr99@gmail.com>
2024-04-13 09:42:42 +0300

    Fixes

--- server/db/db_chat/db_insert_chat.c
+++ server/db/db_chat/db_insert_chat.c
@@ -1,7 +1,7 @@
 #include "../../inc/server.h"
 
 // Function to insert a new chat into the database
-t_response_code insert_chat_into_db(const char *chat_name, int date, int avatar_color, sqlite3 *db)
+t_response_code insert_chat_into_db(const char *chat_name, int date, int avatar_color)
 {
     // Check if the chat already exists in the database
     int chat_id = db_get_chat_id_by_name(chat_name);
@@ -27,9 +27,7 @@ t_response_code insert_chat_into_db(const char *chat_name, int date, int avatar_
 // Original function refactored
 t_response_code db_insert_chat(const char *chat_name, int date, int avatar_color)
 {
-    sqlite3 *db = open_db();
-    t_response_code response = insert_chat_into_db(chat_name, date, avatar_color, db);
-    sqlite3_close(db);
+    t_response_code response = insert_chat_into_db(chat_name, date, avatar_color);
 
     return response;
 }
--- server/db/db_chat/db_modify_chat_name.c
+++ server/db/db_chat/db_modify_chat_name.c
@@ -1,7 +1,7 @@
 #include "../../inc/server.h"
 
 // Function to modify the name of a chat in the database
-t_response_code modify_chat_name_in_db(int chat_id, const char *new_name, sqlite3 *db)
+t_response_code modify_chat_name_in_db(int chat_id, const char *new_name)
 {
     // Check if a chat with the new name already exists
     int chat_id_for_check = db_get_chat_id_by_name(new_name);
@@ -25,9 +25,7 @@ t_response_code modify_chat_name_in_db(int chat_id, const char *new_name, sqlite
 // Original function refactored
 t_response_code db_modify_chat_name(int chat_id, const char *new_name)
 {
-    sqlite3 *db = open_db();
-    t_response_code response = modify_chat_name_in_db(chat_id, new_name, db);
-    sqlite3_close(db);
+    t_response_code response = modify_chat_name_in_db(chat_id, new_name);
 
     return response;
 }
