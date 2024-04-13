57f26982ecf90ee87f2ab1451f0c087aff1e2a66
Maksull <kyrkyrkyr99@gmail.com>
2024-04-13 14:50:29 +0300

    Refactor gui

--- client/gui/update_events/update_chat_field.c
+++ client/gui/update_events/update_chat_field.c
@@ -1,6 +1,28 @@
 #include "../../inc/client.h"
 
-// Update the chat field with messages
+// Function to display a label indicating no messages
+static void display_no_messages_label(GtkWidget *chat_field)
+{
+    GtkWidget *messaging_label = gtk_label_new("No messages here yet...");
+    gtk_widget_set_name(messaging_label, "messaging_label");
+    gtk_widget_set_halign(GTK_WIDGET(messaging_label), GTK_ALIGN_CENTER);
+    gtk_widget_set_valign(GTK_WIDGET(messaging_label), GTK_ALIGN_CENTER);
+    gtk_widget_set_vexpand(messaging_label, TRUE);
+    gtk_widget_set_hexpand(messaging_label, TRUE);
+    gtk_box_pack_start(GTK_BOX(chat_field), messaging_label, FALSE, FALSE, 0);
+}
+
+// Function to add each message to the chat field
+static void add_messages_to_chat_field(GtkWidget *chat_field, t_msg *messages)
+{
+    while (messages)
+    {
+        add_message(messages);
+        messages = messages->next;
+    }
+}
+
+// Function to update the chat field with messages
 void update_chat_field()
 {
     GtkWidget *chat_field = get_widget_by_name_r(main_window, "chat_field"); // Get chat field widget
@@ -8,25 +30,14 @@ void update_chat_field()
     clear_container(chat_field); // Clear the chat field
 
     t_msg *messages = utils->current_chat->messages; // Get messages for the current chat
-    if (mx_msg_list_size(messages) == 0)             // Check if there are no messages
+
+    if (mx_msg_list_size(messages) == 0)
     {
-        // Display a label indicating no messages
-        GtkWidget *messaging_label = gtk_label_new("No messages here yet...");
-        gtk_widget_set_name(messaging_label, "messaging_label");
-        gtk_widget_set_halign(GTK_WIDGET(messaging_label), GTK_ALIGN_CENTER);
-        gtk_widget_set_valign(GTK_WIDGET(messaging_label), GTK_ALIGN_CENTER);
-        gtk_widget_set_vexpand(messaging_label, TRUE);
-        gtk_widget_set_hexpand(messaging_label, TRUE);
-        gtk_box_pack_start(GTK_BOX(chat_field), messaging_label, FALSE, FALSE, 0);
+        display_no_messages_label(chat_field);
     }
     else
     {
-        // Add each message to the chat field
-        while (messages)
-        {
-            add_message(messages);
-            messages = messages->next;
-        }
+        add_messages_to_chat_field(chat_field, messages);
     }
 
     gtk_widget_show_all(chat_field); // Show all widgets in the chat field
--- client/gui/update_events/update_chatlist_item_info.c
+++ client/gui/update_events/update_chatlist_item_info.c
@@ -1,33 +1,76 @@
 #include "../../inc/client.h"
 
-// Update information of a chat list item
-void update_chatlist_item_info(t_chat *chat)
+// Function to prepare text for the chat list item
+static char *prepare_chatlist_item_text(t_msg *last_new_msg)
 {
-    GtkWidget *chatlist_container = get_widget_by_name_r(main_window, "chatlist"); // Get chat list container
-    GtkWidget *chatlist_item = get_widget_by_name_r(chatlist_container, chat->name); // Get chat list item widget
+    if (last_new_msg)
+    {
+        return ellipsis_str(last_new_msg->text, 16);
+    }
+    else
+    {
+        return mx_strdup("No messages yet");
+    }
+}
 
-    // Prepare text and date information for the chat list item
-    char *text = chat->last_new_msg ? ellipsis_str(chat->last_new_msg->text, 16) : mx_strdup("No messages yet");
-    char *date_str = chat->last_new_msg ? chat->last_new_msg->date_str : "";
-    int new_msg_count = chat ? chat->new_msg_count : 0;
+// Function to prepare date string for the chat list item
+static char *prepare_chatlist_item_date(t_msg *last_new_msg)
+{
+    if (last_new_msg)
+    {
+        return last_new_msg->date_str;
+    }
+    else
+    {
+        return "";
+    }
+}
 
+// Function to update the notification count of the chat list item
+static void update_chatlist_item_notify_count(GtkWidget *chatlist_item_notify, int new_msg_count)
+{
+    if (new_msg_count > 0)
+    {
+        gtk_label_set_text(GTK_LABEL(chatlist_item_notify), mx_itoa(new_msg_count));
+        add_class(chatlist_item_notify, "chatlist_item_notify--visible");
+    }
+    else
+    {
+        remove_class(chatlist_item_notify, "chatlist_item_notify--visible");
+    }
+}
+
+// Function to update the labels of a chat list item
+static void update_chatlist_item_labels(GtkWidget *chatlist_item, const char *message_text, const char *time_text)
+{
     // Get widgets for message, time, and notification count of the chat list item
     GtkWidget *chatlist_item_message = get_widget_by_name_r(chatlist_item, "chatlist_item_message");
     GtkWidget *chatlist_item_time = get_widget_by_name_r(chatlist_item, "chatlist_item_time");
+
+    // Set text for message and time labels
+    gtk_label_set_text(GTK_LABEL(chatlist_item_message), message_text);
+    gtk_label_set_text(GTK_LABEL(chatlist_item_time), time_text);
+}
+
+// Function to update information of a chat list item
+void update_chatlist_item_info(t_chat *chat)
+{
+    GtkWidget *chatlist_container = get_widget_by_name_r(main_window, "chatlist");   // Get chat list container
+    GtkWidget *chatlist_item = get_widget_by_name_r(chatlist_container, chat->name); // Get chat list item widget
+
+    // Prepare text and date information for the chat list item
+    char *text = prepare_chatlist_item_text(chat->last_new_msg);
+    char *date_str = prepare_chatlist_item_date(chat->last_new_msg);
+    int new_msg_count = chat ? chat->new_msg_count : 0;
+
+    // Update the labels of the chat list item
+    update_chatlist_item_labels(chatlist_item, text, date_str);
+
+    // Get widget for the notification count of the chat list item
     GtkWidget *chatlist_item_notify = get_widget_by_name_r(chatlist_item, "chatlist_item_notify");
 
-    // Set text and date information for message and time labels
-    gtk_label_set_text(GTK_LABEL(chatlist_item_message), text);
-    gtk_label_set_text(GTK_LABEL(chatlist_item_time), date_str);
+    // Update the notification count
+    update_chatlist_item_notify_count(chatlist_item_notify, new_msg_count);
 
-    if (new_msg_count > 0) // If there are new messages
-    {
-        gtk_label_set_text(GTK_LABEL(chatlist_item_notify), mx_itoa(new_msg_count)); // Set new message count text
-        add_class(chatlist_item_notify, "chatlist_item_notify--visible"); // Add class to make notification visible
-    }
-    else // If there are no new messages
-    {
-        remove_class(chatlist_item_notify, "chatlist_item_notify--visible"); // Remove class to hide notification
-    }
     mx_strdel(&text); // Free memory allocated for text
 }
