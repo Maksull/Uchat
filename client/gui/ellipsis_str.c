793763773af93e24fde95720afa808e063390f35
Maksull <kyrkyrkyr99@gmail.com>
2024-04-13 11:41:17 +0300

    Refactor gui

--- client/gui/ellipsis_str.c
+++ client/gui/ellipsis_str.c
@@ -1,22 +1,22 @@
 #include "../inc/client.h"
 
-// Function to adds an ellipsis
+// Function to add an ellipsis to a string if it exceeds a specified length
 char *ellipsis_str(const char *str, int overflow_len)
 {
-    char *tmp = NULL;    // Temporary pointer for storing a substring
-    char *result = NULL; // Pointer to the resulting string with an ellipsis
-
     // Check if the length of the original string exceeds the specified overflow length
     if (mx_strlen(str) >= overflow_len)
     {
-        tmp = mx_strndup(str, overflow_len); // If it exceeds, create a substring of the original string up to the overflow length
-        result = mx_strjoin(tmp, "...");     // Concatenate an ellipsis (...) to the substring
-        mx_strdel(&tmp);                     // Free the memory allocated for the substring
+        // If it exceeds, create a substring of the original string up to the overflow length
+        char *substring = mx_strndup(str, overflow_len);
+        // Concatenate an ellipsis (...) to the substring
+        char *result = mx_strjoin(substring, "...");
+        // Free the memory allocated for the substring
+        mx_strdel(&substring);
+        return result;
     }
     else
     {
-        result = mx_strdup(str); // If the length of the original string is within the limit, duplicate the original string
+        // If the length of the original string is within the limit, duplicate the original string
+        return mx_strdup(str);
     }
-
-    return result;
 }
