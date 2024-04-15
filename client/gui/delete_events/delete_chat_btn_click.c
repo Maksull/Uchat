8038910cf5d2999880b5a4094543da88dd2bf87e
danilozhornik <danikzhornik2208@gmail.com>
2024-04-15 17:06:54 +0300

    delete_event_refactor

--- client/gui/delete_events/delete_chat_btn_click.c
+++ client/gui/delete_events/delete_chat_btn_click.c
@@ -1,5 +1,17 @@
 #include "../../inc/client.h"
 
+// Function to update chat list and destroy chat window
+void update_and_destroy_chat(GtkWidget *widget) {
+    // Set the current chat pointer to NULL and update the chat list
+    utils->current_chat = NULL;
+    update_chatlist();
+
+    // Destroy the top-level window containing the chat
+    GtkWidget *toplevel = gtk_widget_get_toplevel(widget);
+    gtk_widget_destroy(toplevel);
+}
+
+
 // Function to handle delete chat button click
 void delete_chat_btn_click(GtkWidget *widget, gpointer data)
 {
@@ -8,13 +20,8 @@ void delete_chat_btn_click(GtkWidget *widget, gpointer data)
     // If the deletion is successful, update the chat list and destroy the window
     if (response_code == R_SUCCESS)
     {
-        // Set the current chat pointer to NULL and update the chat list
-        utils->current_chat = NULL;
-        update_chatlist();
-
-        // Destroy the top-level window containing the chat
-        GtkWidget *toplevel = gtk_widget_get_toplevel(widget);
-        gtk_widget_destroy(toplevel);
+        // Call the function to update chat list and destroy chat window
+        update_and_destroy_chat(widget);
     }
     (void)data;
 }
