c74acb29f0886b2668bd4313055f9653030b280b
Maksull <kyrkyrkyr99@gmail.com>
2024-04-15 17:28:12 +0300

    Refactor draw avatars and focus events

--- client/gui/focus_events/focus_out_current_password_field.c
+++ client/gui/focus_events/focus_out_current_password_field.c
@@ -1,15 +1,15 @@
 #include "../../inc/client.h"
 
-// Function to focus out current password field
-void focus_out_current_password_field(GtkWidget *widget, gpointer data)
+// Function to find necessary widgets within the top-level window for focusing out current password field
+void find_password_widgets(GtkWidget *toplevel, GtkWidget **current_password_field, GtkWidget **current_password_notify_label)
 {
-    GtkWidget *toplevel = gtk_widget_get_toplevel(widget); // Get the top-level window containing the widget
-    (void)data;
+    *current_password_field = get_widget_by_name_r(toplevel, "current_password_field");
+    *current_password_notify_label = get_widget_by_name_r(toplevel, "current_password_notify_label");
+}
 
-    // Find the current password field and the corresponding notification label within the top-level window
-    GtkWidget *current_password_field = get_widget_by_name_r(toplevel, "current_password_field");
-    GtkWidget *current_password_notify_label = get_widget_by_name_r(toplevel, "current_password_notify_label");
-    // Validate the content of the current password field
+// Function to validate the content of the current password field and compare it with the user's current password
+void validate_current_password(GtkWidget *current_password_field, GtkWidget *current_password_notify_label)
+{
     if (validate_password_field(current_password_field, current_password_notify_label))
     {
         char *current_password = (char *)gtk_entry_get_text(GTK_ENTRY(current_password_field)); // Get the current password entered by the user
@@ -27,4 +27,18 @@ void focus_out_current_password_field(GtkWidget *widget, gpointer data)
             set_notify_success_style(current_password_notify_label, "");
         }
     }
+}
+
+// Function to focus out current password field
+void focus_out_current_password_field(GtkWidget *widget, gpointer data)
+{
+    GtkWidget *toplevel = gtk_widget_get_toplevel(widget); // Get the top-level window containing the widget
+    (void)data;
+
+    // Find the current password field and the corresponding notification label within the top-level window
+    GtkWidget *current_password_field, *current_password_notify_label;
+    find_password_widgets(toplevel, &current_password_field, &current_password_notify_label);
+
+    // Validate the content of the current password field and compare it with the user's current password
+    validate_current_password(current_password_field, current_password_notify_label);
 }
--- client/gui/focus_events/focus_out_repassword_field.c
+++ client/gui/focus_events/focus_out_repassword_field.c
@@ -1,14 +1,23 @@
 #include "../../inc/client.h"
 
-// Function to focus out repassword field
-void focus_out_repassword_field(GtkWidget *widget, gpointer data)
+// Function to validate the content of the re-enter password field and update its corresponding notification label
+void validate_repassword_field_main_window()
 {
-    if (widget) { }
-    (void)data;
-    
     // Find the password field, re-enter password field, and the corresponding notification label within the main window
     GtkWidget *password_field = get_widget_by_name_r(main_window, "password_field");
     GtkWidget *repassword_field = get_widget_by_name_r(main_window, "repassword_field");
     GtkWidget *repassword_notify_label = get_widget_by_name_r(main_window, "repassword_notify_label");
-    validate_repassword_field(password_field, repassword_field, repassword_notify_label); // Validate the content of the re-enter password field and update the notification label
+    // Validate the content of the re-enter password field and update the notification label
+    validate_repassword_field(password_field, repassword_field, repassword_notify_label);
+}
+
+// Function to handle focus out the re-enter password field
+void focus_out_repassword_field(GtkWidget *widget, gpointer data)
+{
+    if (widget)
+    { }
+    (void)data;
+
+    // Validate the re-enter password field and update its corresponding notification label
+    validate_repassword_field_main_window();
 }
