48dea7ba34c6c4b60b3d57454ab0fa8e4cfcd862
Maksull <kyrkyrkyr99@gmail.com>
2024-04-22 17:21:08 +0300

    Fixes

--- client/gui/load_css.c
+++ client/gui/load_css.c
@@ -4,6 +4,6 @@
 void load_css()
 {
     GtkCssProvider *styles = gtk_css_provider_new(); // Create a new GTK CSS provider
-    gtk_css_provider_load_from_path(styles, "client/data/styles/main.css", NULL); // Load CSS styles from the specified file path
+    gtk_css_provider_load_from_path(styles, "client/data/styles/style.css", NULL); // Load CSS styles from the specified file path
     gtk_style_context_add_provider_for_screen(gdk_screen_get_default(), GTK_STYLE_PROVIDER(styles), GTK_STYLE_PROVIDER_PRIORITY_USER); // Add the CSS provider to the default screen's style context
 }
